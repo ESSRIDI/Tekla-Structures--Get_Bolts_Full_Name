42834941cf629a1ec065d32be1ad2b720614b6aa
ESSRIDI <essridi.pro@gmail.com>
2024-05-30 01:00:11 +0200

    Add project files.

---
+++ GetBoltLength/Form_bolt.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GetBoltLength
+{
+    partial class Form_bolt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bolts_btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bolts_btn
+            // 
+            this.bolts_btn.Location = new System.Drawing.Point(594, 331);
+            this.bolts_btn.Name = "bolts_btn";
+            this.bolts_btn.Size = new System.Drawing.Size(160, 61);
+            this.bolts_btn.TabIndex = 1;
+            this.bolts_btn.Text = "show bolts list";
+            this.bolts_btn.UseVisualStyleBackColor = true;
+            this.bolts_btn.Click += new System.EventHandler(this.bolts_btn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(358, 233);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form_bolt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bolts_btn);
+            this.Name = "Form_bolt";
+            this.Text = "List all bolts";
+            this.Load += new System.EventHandler(this.Form_bolt_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bolts_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ GetBoltLength/Form_bolt.cs
@@ -0,0 +1,147 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
+
+namespace GetBoltLength
+{
+    public partial class Form_bolt : Form
+    {
+        Model _model;
+        DataGridView dataGridView2;
+
+        public Form_bolt()
+        {
+
+            InitializeComponent();
+
+
+            dataGridView2 = new DataGridView();
+
+            _model = new Model();
+        }
+
+        private void Form_bolt_Load(object sender, EventArgs e)
+        {
+
+            try
+
+            {
+
+                if (_model.GetConnectionStatus())
+                {
+
+                    Operation.DisplayPrompt("App is now connected to Tekla Structures");
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Something went wrong !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+
+        }
+
+        private void bolts_btn_Click(object sender, EventArgs e)
+        {
+
+            BoltsReportProperties boltsReportProperties = new BoltsReportProperties();
+            var data = boltsReportProperties.Run();
+
+
+
+
+            dataGridView1.Columns.Add("Bolts", "Bolts");
+
+            foreach (var d in data)
+            {
+                dataGridView1.Rows.Add(d);
+            }
+
+        }
+
+
+    }
+
+
+
+    public class BoltsReportProperties
+    {
+        
+        private readonly ArrayList _reportProperties = new ArrayList { "SCREW_NAME", "SCREW_TYPE" };
+        public ArrayList Run()
+        {
+
+            Model _model = new Model();
+            ModelObjectEnumerator modelObjectEnum = _model.GetModelObjectSelector().GetAllObjects();
+
+
+            var bolts = modelObjectEnum.ToList();
+
+
+            var boltsProperties = bolts.OfType<BoltArray>().Select(b =>
+            {
+                var properties = new Hashtable();
+                b.GetStringReportProperties(_reportProperties, ref properties);
+
+                return new
+                {
+                    bolt = b,
+                    StringProps = properties,
+                };
+            }).ToList();
+
+            var json = JsonConvert.SerializeObject(boltsProperties);
+            //test
+            Console.WriteLine(json);
+
+            var boltsNames = new ArrayList();
+
+            foreach (var entry in boltsProperties)
+
+            {
+                string boltN = entry.StringProps["SCREW_NAME"].ToString();
+                boltsNames.Add(boltN);
+
+
+            }
+
+
+
+            return boltsNames;
+        }
+
+
+
+
+    }
+
+
+    public static class TeklaExxtensions
+    {
+
+        public static IList<ModelObject> ToList(this ModelObjectEnumerator enumerator)
+        {
+            var listObjects = new List<ModelObject>();
+            while (enumerator.MoveNext())
+            {
+                var obj = enumerator.Current;
+                if (obj == null) continue;
+
+                listObjects.Add(obj);
+            }
+            return listObjects;
+        }
+    }
+
+}
